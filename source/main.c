0a30ee7a96d42325f2535b175b03b1934979a262
Cruz Ramirez <crami119@wch136-24.cs.ucr.edu>
2021-04-15 21:55:28 -0700

    Working on Exercise 1

--- source/main.c
+++ source/main.c
@@ -12,11 +12,75 @@
 #include "simAVRHeader.h"
 #endif
 
-int main(void) {
-    /* Insert DDR and PORT initializations */
 
-    /* Insert your solution below */
+enum LED_States{LED_Start, LED_ZeroOn, LED_ZeroOnWait, LED_OneOn, LED_OneOnWait}LED_State;
+
+void TickFct_LED(){
+
+        switch(LED_State){ //transition state
+        case LED_Start:
+        LED_State = LED_ZeroOn;
+        break;
+
+        case LED_ZeroOn:
+        if(PINA == 0x00){
+        LED_State = LED_ZeroOnWait;
+        }
+        break;
+
+        case LED_ZeroOnWait:
+        if(PINA == 0x01){
+        LED_State = LED_OneOn;
+        }
+        break;
+
+        case LED_OneOn:
+        if(PINA == 0x00){
+        LED_State = LED_OneOnWait;
+        }
+        break;
+
+        case LED_OneOnWait:
+        if(PINA == 0x01){
+        LED_State = LED_ZeroOn;
+        }
+        break;
+
+        default:
+        break;
+        }
+
+	switch(LED_State){ //state actions
+
+	case LED_ZeroOn:
+	PORTB = 0x01;
+	break;
+
+	case LED_OneOn:
+	PORTB = 0x02;
+	break;
+
+	case LED_Start:
+	case LED_ZeroOnWait:
+	case LED_OneOnWait:
+	default:
+	break;
+
+	}
+
+}
+
+
+int main(void) {
+    
+	DDRA = 0x00;
+	DDRB = 0xFF;
+
+	LED_State = LED_Start;
+
     while (1) {
+
+	TickFct_LED();
 
     }
     return 1;
