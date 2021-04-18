1fb60ab1c8da55029b846dfdb28dbd24c467e3d2
Cruz Ramirez <crami119@wch136-24.cs.ucr.edu>
2021-04-18 00:53:20 -0700

    Creating code for part 5

--- source/main.c
+++ source/main.c
@@ -13,58 +13,59 @@
 #endif
 
 
+	unsigned char i = 0x00;
+        char unlockSeq[4]{0x04, 0x01, 0x02, 0x01};
+        char userInput[4]{0x00, 0x00, 0x00, 0x00};
 
-enum DB_States{DB_Wait, DB_ToUnlock1, DB_ToUnlock2, DB_Unlock, DB_Lock}DB_State;
+
+
+
+enum DB_States{DB_Wait, DB_Unlock, DB_Lock}DB_State;
 
 void TickFct_DB(){
 
         switch(DB_State){ //transition state
-
-        case DB_Wait:
-        if(PINA == 0x04){
-        DB_State = DB_ToUnlock1;
-        }
+     
+	case DB_Wait:
+	if(unlockSeq == userInput){
+	DB_State = DB_Unlock; 
+	}
+	else if(PINA == 0x00){
+	DB_State = DB_Wait;
+	}
 	else if((PINA >> 7) == 0x01){
 	DB_State = DB_Lock;
 	}
-        break;
+	break;
 
-        case DB_ToUnlock1:
-	if(PINA == 0x00){
-	DB_State = DB_ToUnlock2;
-	}
-	else if(PINA == 0x04){
-	DB_State = DB_ToUnlock1;
-	}
-	else{
-	DB_State = DB_Wait;
-	}
-        break;
-
-	case DB_ToUnlock2:
-	if((PINA == 0x02) && (PORTB == 0x00)){
-	DB_State = DB_Unlock;
-	}
-	else if((PINA == 0x02) && (PORTB == 0x01)){
-	DB_State = DB_Lock;	
-	}
-	else if(PINA == 0x00){
-	DB_State = DB_ToUnlock2;
-	}
-	else{
-	DB_State = DB_Wait;
-	}
+	case DB_Unlock:	
+	DB_State = DB_Wait;	
 	break;
 
 	case DB_Lock:
-	case DB_Unlock:
 	DB_State = DB_Wait;
+	break;
 
-        default:
-        break;
-        }
+	default:
+	break;
+	}
 
 	switch(DB_State){ //state actions
+
+	case DB_Wait:
+	if(userInput[3] != 0x00){
+
+	userInput[0] = 0x00;
+	userInput[1] = 0x00;
+	userInput[2] = 0x00;
+	userInput[3] = 0x00;
+	i = 0x00;
+	}
+	else if(PINA != 0x00){
+	userInput[i] = PINA;
+	++i;
+	}
+	break;
 
 	case DB_Unlock:
 	PORTB = 0x01;
@@ -74,14 +75,10 @@ void TickFct_DB(){
 	PORTB = 0x00;
 	break;
 
-	case DB_Wait:
-	case DB_ToUnlock1:
-	case DB_ToUnlock2:
 	default:
 	break;
-
+	
 	}
-
 }
 
 
